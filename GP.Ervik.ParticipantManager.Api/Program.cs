8c5d8dfffa1df40aef912edb043cb3f3a243e755
Thomas Ervik <ervikth@gmail.com>
2023-12-23 16:22:28 +0100

    Implement MongoDB Configuration Handling in Program.cs
    - Retrieve MongoDB configuration settings (connection string and database name) from `appsettings.json` and map them to the `MongoDbSettings` class.
    - Implement validation checks for MongoDB settings to ensure that both the connection string and database name are provided.
    - Configure the dependency injection container to add `MongoDbContext` with the MongoDB settings.

--- GP.Ervik.ParticipantManager.Api/Program.cs
+++ GP.Ervik.ParticipantManager.Api/Program.cs
@@ -1,4 +1,9 @@
 
+using GP.Ervik.ParticipantManager.Api.Exceptions;
+using GP.Ervik.ParticipantManager.Api.Settings;
+using GP.Ervik.ParticipantManager.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace GP.Ervik.ParticipantManager.Api
 {
     public class Program
@@ -7,7 +12,22 @@ namespace GP.Ervik.ParticipantManager.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // This includes the connection string and database name required for setting up the MongoDB connection.
+            var mongoDBSettings = builder.Configuration.GetSection("MongoDB").Get<MongoDbSettings>();
+
+            // Validate MongoDB settings
+            if (mongoDBSettings == null ||
+                string.IsNullOrEmpty(mongoDBSettings.ConnectionString) ||
+                string.IsNullOrEmpty(mongoDBSettings.DatabaseName))
+            {
+                throw new ConfigurationException("MongoDB configuration is missing or incomplete.");
+            }
+
             // Add services to the container.
+            builder.Services.AddDbContext<MongoDbContext>(options =>
+            {
+                options.UseMongoDB(mongoDBSettings.ConnectionString, mongoDBSettings.DatabaseName);
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
