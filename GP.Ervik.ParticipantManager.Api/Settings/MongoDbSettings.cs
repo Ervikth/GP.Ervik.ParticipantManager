0f0876823adb880c2f3a823f5d55a3d84c55c57d
Thomas Ervik <ervikth@gmail.com>
2023-12-23 16:17:52 +0100

    Add MongoDbSettings Configuration Class
    - Create a new `MongoDbSettings` class within the Settings folder.
    - This class includes properties for `ConnectionString` and `DatabaseName`.
    - It is for mapping MongoDB configuration settings from files such as `appsettings.json`.
    - This simplifies the process of accessing MongoDB configuration throughout the application

---
+++ GP.Ervik.ParticipantManager.Api/Settings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace GP.Ervik.ParticipantManager.Api.Settings
+{
+    public class MongoDbSettings
+    {
+        public string? ConnectionString { get; set; }
+        public string? DatabaseName { get; set; }
+    }
+}
