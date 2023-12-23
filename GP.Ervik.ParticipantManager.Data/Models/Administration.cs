1e55f1d18fa3b2f78b0bdfe49ce97096e1b121eb
Thomas Ervik <ervikth@gmail.com>
2023-12-23 15:56:09 +0100

    Add Administration and Participant Models in Data Project
    - Create the `Administration` class in Models/Administration.cs:
      - Includes properties for:
        - Id (ObjectId)
        - Name (string)
        - Email (string)
        - Password (string)
    
    - Create the `Participant` class in Models/Participant.cs:
      - Includes properties for:
        - Id (ObjectId)
        - Name (string)
        - Email (string)
        - PhoneNumber (string)
        - Allergens (string)
        - Comment (string)

---
+++ GP.Ervik.ParticipantManager.Data/Models/Administration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GP.Ervik.ParticipantManager.Data.Models
+{
+    public class Administration
+    {
+        public ObjectId Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
