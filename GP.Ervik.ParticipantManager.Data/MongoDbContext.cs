1daddf1fadeb05a57e8683ae6eae868e684f1aaa
Thomas Ervik <ervikth@gmail.com>
2023-12-23 16:00:57 +0100

    Implement MongoDbContext for MongoDB Integration
    - Add a new `MongoDbContext` class within the Data project.
    - Define DbSets for `Administration` and `Participant`.
    - In the constructor, accept `DbContextOptions<MongoDbContext>` to configure the context.
    - Override `OnModelCreating` to configure MongoDB collections
      - Map `Participant` to the 'participant' collection
      - Map `Administration` to the 'administration' collection

---
+++ GP.Ervik.ParticipantManager.Data/MongoDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GP.Ervik.ParticipantManager.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace GP.Ervik.ParticipantManager.Data
+{
+    public class MongoDbContext : DbContext
+    {
+        public DbSet<Administration>? Administrations { get; set; }
+        public DbSet<Participant>? Participants { get; set; }
+        public MongoDbContext(DbContextOptions<MongoDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Participant>(u =>
+            {
+                u.ToCollection("participant");
+                u.HasKey(v => v.Id);
+                u.Property(v => v.Name).HasElementName("name");
+                u.Property(v => v.Email).HasElementName("email");
+                u.Property(v => v.PhoneNumber).HasElementName("phonenumber");
+                u.Property(v => v.Allergens).HasElementName("allergens");
+                u.Property(v => v.Comment).HasElementName("comment");
+            });
+            modelBuilder.Entity<Administration>(u =>
+            {
+                u.ToCollection("administration");
+                u.HasKey(v => v.Id);
+                u.Property(v => v.Name).HasElementName("name");
+                u.Property(v => v.Email).HasElementName("email");
+                u.Property(v => v.Password).HasElementName("password");
+            });
+        }
+    }
+}
