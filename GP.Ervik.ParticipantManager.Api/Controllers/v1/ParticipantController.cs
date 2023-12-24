feb51d50fcc2a5efdb00252fb408aba8f5d5828c
Thomas Ervik <ervikth@gmail.com>
2023-12-24 01:02:29 +0100

    Refactor ParticipantController with Exception Handling and Logging
    1. Wrapped all actions (Get, GetParticipant, AddParticipant, UpdateParticipant, DeleteParticipant) in try-catch blocks to handle potential exceptions.
    2. Fixed the CreatedAtAction in the AddParticipant method to ensure the participantId is correctly formatted as a string in the response.

--- GP.Ervik.ParticipantManager.Api/Controllers/v1/ParticipantController.cs
+++ GP.Ervik.ParticipantManager.Api/Controllers/v1/ParticipantController.cs
@@ -23,89 +23,153 @@ namespace GP.Ervik.ParticipantManager.Api.Controllers.v1
         [HttpGet, Authorize]
         public async Task<ActionResult<List<ParticipantDto>>> Get()
         {
-            var response = (await _mongoContext.Participants.ToListAsync())
-                .Select(part => new ParticipantDto
+            try
+            {
+                _logger.LogInformation("Retrieving all participants");
+
+                    var response = (await _mongoContext.Participants.ToListAsync())
+                    .Select(part => new ParticipantDto
+                    {
+                        Id = part.Id.ToString(),
+                        Name = part.Name,
+                        Email = part.Email,
+                        PhoneNumber = part.PhoneNumber,
+                        Allergens = part.Allergens,
+                        Comment = part.Comment
+                    });
+                    _logger.LogInformation("Retrieved participants");
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while retrieving participants");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpGet("{participantId}"), Authorize]
+        public async Task<IActionResult> GetParticipant(string participantId)
+        {
+            try
+            {
+                _logger.LogInformation("Getting participant");
+
+                var participant = await _mongoContext.Participants.FindAsync(ObjectId.Parse(participantId));
+
+                if (participant == null)
                 {
-                    Id = part.Id.ToString(),
-                    Name = part.Name,
-                    Email = part.Email,
-                    PhoneNumber = part.PhoneNumber,
-                    Allergens = part.Allergens,
-                    Comment = part.Comment
-                });
-            return Ok(response);
-        }
+                    _logger.LogWarning("Participant with ID not found.");
+                    return NotFound("Participant with ID not found.");
+                }
 
-        [HttpGet("userId"), Authorize]
-        public async Task<IActionResult> GetParticipant(string userId)
-        {
-            var user = await _mongoContext.Participants.FindAsync(ObjectId.Parse(userId));
+                var participantDto = new ParticipantDto
+                {
+                    Id = participant.Id.ToString(),
+                    Name = participant.Name,
+                    Email = participant.Email,
+                    PhoneNumber = participant.PhoneNumber,
+                    Allergens = participant.Allergens,
+                    Comment = participant.Comment
+                };
 
-            if (user == null)
-            {
-                return NotFound($"User with ID {userId} not found.");
+                _logger.LogInformation("Participant retrieved successfully");
+                return Ok(participantDto);
             }
-
-            var userDto = new ParticipantDto
+            catch (Exception ex)
             {
-                Id = user.Id.ToString(),
-                Name = user.Name,
-                Email = user.Email,
-                PhoneNumber = user.PhoneNumber,
-                Allergens = user.Allergens,
-                Comment = user.Comment
-            };
-            return Ok(userDto);
+                _logger.LogError(ex, "An error occurred while retrieving participant with ID.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
+
         [HttpPost, Authorize]
-        public async Task<IActionResult> AddParticipant(ParticipantCreateDto participantCreateDto)
+        public async Task<ActionResult<ParticipantCreateDto>> AddParticipant(ParticipantCreateDto participantCreateDto)
         {
-            var participant = new Participant
+            try
             {
-                Id = ObjectId.GenerateNewId(),
-                Name = participantCreateDto.Name,
-                Email = participantCreateDto.Email,
-                PhoneNumber = participantCreateDto.PhoneNumber,
-                Allergens = participantCreateDto.Allergens,
-                Comment = participantCreateDto.Comment
-            };
-            await _mongoContext.Participants.AddAsync(participant);
-            await _mongoContext.SaveChangesAsync();
+                _logger.LogInformation("Adding new participant.");
 
-            return CreatedAtAction(nameof(GetParticipant), new { id = participant.Id }, participant);
-        }
-        [HttpPatch("{userId}"), Authorize]
-        public async Task<IActionResult> UpdateParticipant(string userId, ParticipantDto participantDto)
-        {
-            var participant = await _mongoContext.Participants.FindAsync(ObjectId.Parse(userId));
-            if (participant == null)
-            {
-                return NotFound($"User with ID {userId} not found.");
+                var participant = new Participant
+                {
+                    Id = ObjectId.GenerateNewId(),
+                    Name = participantCreateDto.Name,
+                    Email = participantCreateDto.Email,
+                    PhoneNumber = participantCreateDto.PhoneNumber,
+                    Allergens = participantCreateDto.Allergens,
+                    Comment = participantCreateDto.Comment
+                };
+
+                await _mongoContext.Participants.AddAsync(participant);
+                await _mongoContext.SaveChangesAsync();
+
+                _logger.LogInformation("Participant added successfully");
+
+                return CreatedAtAction(nameof(GetParticipant), new { participantId = participant.Id.ToString() }, participant);
             }
-
-            if (participantDto != null) participant.Name = participantDto.Name;
-            if (participantDto.Email != null) participant.Email = participantDto.Email;
-            if (participantDto.PhoneNumber != null) participant.PhoneNumber = participantDto.PhoneNumber;
-            if (participantDto.Allergens != null) participant.Allergens = participantDto.Allergens;
-            if (participantDto.Comment != null) participant.Comment = participantDto.Comment;
-            _mongoContext.Participants.Update(participant);
-            await _mongoContext.SaveChangesAsync();
-
-            return Ok(participant);
-        }
-        [HttpDelete("{userId}"), Authorize]
-        public async Task<IActionResult> DeleteUser(string userId)
-        {
-            var user = await _mongoContext.Participants.FindAsync(ObjectId.Parse(userId));
-            if (user == null)
+            catch (Exception ex)
             {
-                return NotFound($"User with ID {userId} not found.");
+                _logger.LogError(ex, "Error occurred when adding participant.");
+                return StatusCode(500, "An error occurred while processing your request.");
             }
-
-            _mongoContext.Participants.Remove(user);
-            await _mongoContext.SaveChangesAsync();
-            return NoContent();
         }
 
+        [HttpPatch("{participantId}"), Authorize]
+        public async Task<IActionResult> UpdateParticipant(string participantId, ParticipantDto participantDto)
+        {
+            try
+            {
+                _logger.LogInformation("Attempting to update participant.");
+                var participant = await _mongoContext.Participants.FindAsync(ObjectId.Parse(participantId));
+                if (participant == null)
+                {
+                    _logger.LogWarning("Participant not found - ID: {ParticipantId}", participantId);
+                    return NotFound($"Participant with ID {participantId} not found.");
+                }
+                if (participantDto != null) participant.Name = participantDto.Name;
+                if (participantDto.Email != null) participant.Email = participantDto.Email;
+                if (participantDto.PhoneNumber != null) participant.PhoneNumber = participantDto.PhoneNumber;
+                if (participantDto.Allergens != null) participant.Allergens = participantDto.Allergens;
+                if (participantDto.Comment != null) participant.Comment = participantDto.Comment;
+
+                _mongoContext.Participants.Update(participant);
+                await _mongoContext.SaveChangesAsync();
+
+                _logger.LogInformation("Participant updated successfully - ID: {ParticipantId}", participantId);
+
+                return Ok(participant);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while updating participant - ID: {ParticipantId}", participantId);
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+        
+        [HttpDelete("{participantId}"), Authorize]
+        public async Task<IActionResult> DeleteParticipant(string participantId)
+        {
+            try
+            {
+                _logger.LogInformation("Trying to delete participant - ID: {ParticipantId}", participantId);
+                var participant = await _mongoContext.Participants.FindAsync(ObjectId.Parse(participantId));
+                if (participant == null)
+                {
+                    _logger.LogWarning("Participant not found - ID: {ParticipantId}", participantId);
+                    return NotFound($"Participant with ID {participantId} not found.");
+                }
+
+                _mongoContext.Participants.Remove(participant);
+                await _mongoContext.SaveChangesAsync();
+
+                _logger.LogInformation("Participant deleted successfully - ID: {ParticipantId}", participantId);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while deleting participant - ID: {ParticipantId}", participantId);
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
     }
 }
